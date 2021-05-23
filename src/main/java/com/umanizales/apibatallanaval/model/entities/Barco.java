033a53ea241ccd1ce8fdaf5f94426d5cc84de1bf
Yon Aristizabal <ygaristizabal81046@umanizales.edu.co>
2021-05-23 18:20:51 -0500

    Conexión con BD funcionando

---
+++ src/main/java/com/umanizales/apibatallanaval/model/entities/Barco.java
@@ -0,0 +1,51 @@
+package com.umanizales.apibatallanaval.model.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "barco", schema = "public", catalog = "batalla_naval")
+public class Barco {
+    private int id;
+    private String nombre;
+    private short numeroCasillas;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "nombre", nullable = false, length = 30)
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Basic
+    @Column(name = "numero_casillas", nullable = false)
+    public short getNumeroCasillas() {
+        return numeroCasillas;
+    }
+
+    public void setNumeroCasillas(short numeroCasillas) {
+        this.numeroCasillas = numeroCasillas;
+    }
+
+    @Override
+    public String toString() {
+        return "Barco{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", numeroCasillas=" + numeroCasillas +
+                '}';
+    }
+}
